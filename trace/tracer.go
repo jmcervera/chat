9a63db13a31df980719197d90288e8c31f1e1e0a
Juanma <juanma.cervera@cabify.com>
2020-06-20 12:25:02 +0200

    improvers trace package documentation

--- trace/tracer.go
+++ trace/tracer.go
@@ -11,22 +11,28 @@ type Tracer interface {
 	Trace(...interface{})
 }
 
-type tracer struct {
-	out io.Writer
-}
-
-// New is the trazer constructor
+// New creates a new Tracer that will write the output to
+// the specified io.Writer
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
 }
 
+// tracer is a Tracer that writes to an
+// io.Writer.
+type tracer struct {
+	out io.Writer
+}
+
+// Trace writes the arguments to this Tracers io.Writer.
 func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
 }
 
+// niltracer
 type nilTracer struct{}
 
+// Trace for a nil tracer does nothing
 func (t *nilTracer) Trace(a ...interface{}) {}
 
 // Off creates a Tracer what will ingnore calls to Trace
